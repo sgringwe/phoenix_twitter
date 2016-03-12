2e121439d52d12e01fc72d6f13f055a4b5faa2d7
Scott Ringwelski <sgringwe@mtu.edu>
2016-03-12 14:37:51 -0800

    fix prod config

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ config :phoenix_twitter, PhoenixTwitter.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
-config :hello_phoenix, PhoenixTwitter.Repo,
+config :phoenix_twitter, PhoenixTwitter.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: 20
